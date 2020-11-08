04fe2caa02ac48e665a0168ae1df1ae36a2cb9a1
loganathanc1603 <loganathan.chinnasaamy@gmail.com>
2020-11-08 02:02:53 +0000

    Initial commit for flexible layout app

---
+++ webapp/controller/DetailObjectNotFound.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+	"./BaseController"
+], function (BaseController) {
+	"use strict";
+
+	return BaseController.extend("com.sap.fcl.EmployeeManagement.controller.DetailObjectNotFound", {});
+});
